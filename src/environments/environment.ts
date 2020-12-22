d36b875e59c45c2f5513dd67d2211bde0dd792a9
Bruno Lončar <bruno.loncar@aol.com>
2020-12-22 19:29:04 +0100

    Popravak kod notifyUrl-a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: false,
-  // apiEndpoint: "https://localhost:44386/smartHome",
-  // notifyUrl: "https://localhost:44386/notify",
-  apiEndpoint: "https://smarthome20201102203943.azurewebsites.net/smartHome",
-  notifyUrl: "https://smarthome20201102203943.azurewebsites.net/notify",
+  apiEndpoint: "https://localhost:44386/smartHome",
+  notifyUrl: "https://localhost:44386/notify",
 };
